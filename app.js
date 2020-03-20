444b87ab891d0f0abae81416b4d503898b4e2552
virghiu Bann <virghiu@gmail.com>
2020-03-20 17:45:09 -0400

    implents MVC

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-// const expressHbs = require("express-handlebars");
+
+const errorController = require('./controllers/error')
 
 const app = express();
 
@@ -15,11 +16,9 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render("404", { pageTitle: "Page not found" });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
